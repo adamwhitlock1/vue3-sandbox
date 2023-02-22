83b00de80780dd1957b406f565d5ea16c85e8185
Adam Whitlock <adam.whitlock@adhocteam.us>
2023-02-21 20:51:30 -0700

    first commit

---
+++ src/components/AccordionWrapper.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps(["accordionItems", "selectAccordion"]);
+</script>
+
+<template>
+  <div class="accordion-wrapper">
+    <div
+      class="accordion-item my-1"
+      v-for="(item, index) in accordionItems"
+      :key="index"
+    >
+      <div
+        class="bg-gray-200 flex items-center justify-between py-2 px-4 cursor-pointer"
+        @click="selectAccordion(index)"
+      >
+        <h3 class="text-lg">{{ item.title }}</h3>
+        <div class="w-6 h-6" :class="item.active ? 'rotate-180' : ''">
+          <img src="../assets/arrow-down.svg" alt="arrow-down" />
+        </div>
+      </div>
+      <div :class="item.active ? 'block' : 'hidden'">
+        <p class="px-2 py-4">{{ item.text }}</p>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { ref, reactive } from "vue";
+import AccordionWrapper from "./AccordionWrapper.vue";
+
+const state = reactive({
+  accordionItems: [
+    {
+      title: "What is a dog?",
+      text: "A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.",
+      active: true,
+    },
+    {
+      title: "What kinds of dogs are there?",
+      text: "There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.",
+      active: false,
+    },
+    {
+      title: "How do you acquire a dog?",
+      text: "Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.",
+      active: false,
+    },
+  ],
+});
+
+const selectAccordion = (index) => {
+  state.accordionItems.forEach((item, i) => {
+    if (i === index) {
+      item.active = !item.active;
+    } else {
+      item.active = false;
+    }
+  });
+};
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <AccordionWrapper
+    :accordionItems="state.accordionItems"
+    :selectAccordion="selectAccordion"
+  />
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
